935fe5f45eb5875a754ed1b646ba5a4c65997485
LeiTechCorp <128210341+SearchSerpent@users.noreply.github.com>
2023-05-02 08:00:09 +0800

    for pwa

---
+++ images/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "my-pwa-cache-v1";
+const urlsToCache = [
+  "/index.php",
+  "css/style.css",
+  "js/script.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
--- index.php
+++ index.php
@@ -40,6 +40,17 @@ unset($_SESSION['message']);
         })
     </script>
 
+<script>
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(registration => console.log("Service worker registered"))
+      .catch(error => console.error("Service worker registration failed", error));
+  });
+}
+</script>
+
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
